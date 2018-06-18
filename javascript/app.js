b465a81458d51017a35734b9782aa809693af06d
natgonzalezrosa <natgonzalezrosa@gmail.com>
2018-06-17 21:34:12 -0400

    added changeImage function

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,65 @@
 // Initial array of cartoons for the buttons at the top of the page
 var topics = ["spongeBob squarePants", "rugrats", "the simpsons", "ren and stimpy", "tom and jerry", "adventure time", "adult swim", "family guy", "southpark", "hey arnold", "rick and morty", "robot chicken"];
 
+// Function that will display the JSON content for each button into the #cartoons div
+function displayCartoonInfo () {
+
+    $("#cartoons").empty();
+
+    var cartoon = $(this).attr("data-name");
+
+    var limit = 10;
+
+    // My queryURL for Giphy API using the API key provided
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoon + "&limit=" + limit + "&api_key=f5vRa7rEx6OYgSYr44q3kDC0AAyxaXkF";
+
+    // Performing GET request to the Giphy API and displaying the response in the #cartoons div as a JSON string
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        for (var j = 0; j < limit; j++) {
+
+            // Creates a div to hold the cartoon
+            var cartoonDiv = $("<div class='holdCartoons'>");
+
+            // Creates an element to hold the image
+            var image = $("<img>").addClass("gif");
+            
+            // Adds a src attribute to the image
+            image.attr("src", response.data[j].images.fixed_height_still.url);
+
+            // Adds data-still attribute to the image
+            image.attr("data-still", response.data[j].images.fixed_height_still.url);
+
+            // Adds data-animate attribute  to the image
+            image.attr("data-animate", response.data[j].images.fixed_height.url);
+
+            // Adds data-state attribute to the image
+            image.attr("data-state", "still");
+
+            // Appends the image
+            cartoonDiv.append(image);
+
+            var Rating = response.data[j].rating;
+            console.log(response);
+
+            // Stores the rating data
+            var Rating = response.data[j].rating;
+
+            // Creates a paragraph element to display the rating
+            var pRating = $("<p>").text("Rating: " + Rating);
+
+            // Displays the rating
+            cartoonDiv.append(pRating);
+
+            // Puts the cartoon above the previous cartoons
+            $("#cartoons").prepend(cartoonDiv);
+        }
+    });
+}
+
 // Function for rendering the buttons
 function renderButtons () {
 
@@ -28,49 +87,21 @@ function renderButtons () {
     }
 }
 
-// Function that will display the JSON content for each button into the #cartoons div
-function displayCartoonInfo () {
+function changeImage() {
 
-    var cartoon = $(this).attr("data-name");
+    var state = $(this).attr("data-state");
+    var animateImage = $(this).attr("data-animate");
+    var stillImage = $(this).attr("data-still");
 
-    // My queryURL for Giphy API using the API key provided
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + cartoon + "&api_key=f5vRa7rEx6OYgSYr44q3kDC0AAyxaXkF&limit=10";
+    if (state === "still") {
+        $(this).attr("src", animateImage);
+        $(this).attr("data-state", "animate");
+    }
 
-    // Performing GET request to the Giphy API and displaying the response in the #cartoons div as a JSON string
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-
-        for (var i = 0; i < topics.length; i++) {
-
-            // Creates a div to hold the cartoon
-            var cartoonDiv = $("<div class='holdCartoons'>");
-
-            // Stores the rating data
-            var Rating = response.data[i].rating;
-            console.log(Rating);
-
-            // Creates a paragraph element to display the rating
-            var paraElement = $("<p>").text("Rating: " + Rating);
-
-            // Displays the rating
-            cartoonDiv.append(paraElement);
-
-            // Retrieves the URL for the image
-            var imgURL = response.data[i].images.fixed_height_still.url;
-            console.log(imgURL);
-
-            // Creates an element to hold the image
-            var image = $("<img>").attr("src", imgURL);
-
-            // Appends the image
-            cartoonDiv.append(image);
-
-            // Puts the cartoon above the previous cartoons
-            $("#cartoons").prepend(cartoonDiv);
-        }
-    });
+    else {
+        $(this).attr("src", stillImage);
+        $(this).attr("data-state", "still");
+    }
 }
 
 // This will handle events when the Add a cartoon button is clicked
@@ -85,10 +116,15 @@ $("#addCartoon").on("click", function(event) {
 
     // Calls the renderButtons which will handle the processing of the topics array
     renderButtons();
+
+    $("#cartoon-input").val("");
 });
+
 
 // This will add event listeners to dynamically added elements (in this case, buttons)
 $(document).on("click", ".cartoon", displayCartoonInfo);
+
+$(document).on("click", ".gif", changeImage);
 
 // Adds the initial buttons by calling this function
 renderButtons();
